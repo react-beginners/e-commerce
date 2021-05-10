d6599278f2e00e0dc8274a773d5fc9d6d221f586
Lethin <balaji198932@gmail.com>
2021-05-10 07:57:32 +0530

    Directory Component included

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'; 
 import Header from './components/header/header.component';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Switch>
+        <Route path='/' component={HomePage}/>
+      </Switch>
     </React.Fragment>
   );
 }
