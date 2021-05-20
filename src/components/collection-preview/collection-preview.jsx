df81d714ddb3d45e1b6606b7efe1046f3051e060
Lethin <balaji198932@gmail.com>
2021-05-20 08:08:42 +0530

    Shop page completed

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
+import '../collection-preview/collection-preview.styles.css';
+
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({title, items}) => {
     return(
         <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-    {/*    {items.map(item => (
-        <div key={item.id}>{item.name} </div>
-        ))}  */}
+   {/* {items.map(item => (
+     <div key={item.id}>{item.name}</div>
+   ))} */}
 
-{/*  { items
+{/* { items
   .filter((item, idx) => idx < 4)
   .map(item => (
 <div key={item.id}>{item.name} </div>
-))}   */}
+))} */}
 
 {items
-        .filter((item, idx) => idx < 4)
+        .filter((item, index) => index < 4)
         .map(({ id, ...otherItemProps }) => (
-          <div key={id} {...otherItemProps} />
-        ))}  
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
     );
