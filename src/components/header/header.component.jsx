907c1d6a0a9be952ebe9aad7e5f3cc270f20007e
Lethin <balaji198932@gmail.com>
2021-05-31 07:17:59 +0530

    removed Signin and signout link from header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,13 +14,13 @@ const Header = ({currentUser}) => {
         <Link className='option' to='/shop'>
           SHOP
         </Link>
-        {currentUser ? (
+        {/* {currentUser ? (
             <div className='option' style={{cursor:'pointer'}} onClick={ () => auth.signOut() }>SIGN OUT</div>
           ) : (
             <Link className='option' to='/signin'>
               SIGN-IN
             </Link>
-          )}
+          )} */}
       </div>
     </div>
   );
